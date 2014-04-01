64b859babed1a3abbab165b75e6743613cb197d9
Jon Cheng <jon.syndicate@gmail.com>
2014-04-01 05:44:58 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewGitApp::Application.config.secret_token = '46e6d7499cded75e64d9702769834e93f72b90f21ce61fc1c75d445390a158e30c62b0c143414fdaf3f7b9b6ad808007c9c50f3733872372074f14c2cecfa1f8'
